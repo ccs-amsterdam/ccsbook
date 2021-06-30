319fe7d5c8eb006f3da343160210d769890a384e
Wouter van Atteveldt <wouter@vanatteveldt.com>
2021-06-30 22:58:00 +0200

    print statistics

--- tools/check_snippets.py
+++ tools/check_snippets.py
@@ -1,8 +1,14 @@
 import sys
 
+nerr = 0
+files = set()
 for f in sys.argv[1:]:
     for i, line in enumerate(open(f)):
         line = line.rstrip("\n")
         if len(line) > 50:
             print(f"[{len(line):3}] {f}:{i}")
+            files.add(f)
+            nerr += 1
+
+print(f"** {nerr} problems in {len(files)} files")
 
