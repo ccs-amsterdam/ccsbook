32d45f95f91a4ab33bd5199accc20f2f755c5ca8
Wouter van Atteveldt <wouter@vanatteveldt.com>
2021-07-05 12:52:30 +0200

    Add checks for https and single quote

--- tools/check_snippets.py
+++ tools/check_snippets.py
@@ -6,9 +6,14 @@ for f in sys.argv[1:]:
     for i, line in enumerate(open(f)):
         line = line.rstrip("\n")
         if len(line) > 50:
-            print(f"[{len(line):3}] {f}:{i}")
+            print(f"[TOO LONG {len(line):3}] {f}:{i}")
             files.add(f)
             nerr += 1
+        if "'" in line:
+            print(f"[SINGLE QUOTE] {f}:{i}")
+            nerr += 1
+        if "http:" in line:
+            print(f"[NO HTTPS    ] {f}:{i}")
 
 print(f"** {nerr} problems in {len(files)} files")
 
