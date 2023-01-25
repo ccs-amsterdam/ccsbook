1e801b1cb4f3bf5e05932bb84f54393488e3bd55
Wouter van Atteveldt <wouter@vanatteveldt.com>
2023-01-25 21:25:06 +0100

    commit

--- tools/render_quarto.py
+++ tools/render_quarto.py
@@ -1,5 +1,6 @@
 import logging
 import shutil
+import sys
 from io import StringIO
 from pathlib import Path
 import argparse
@@ -7,39 +8,34 @@ import argparse
 from TexSoup import TexSoup
 from jinja2 import Template
 
-from texhtml.texparser import preprocess, Parser
-from texhtml.toc import TOC
-from texhtml.util import get_template
+from tools.texquarto.texparser import preprocess, Parser
+from tools.texquarto.toc import TOC
+from tools.texquarto.util import get_template
 from tools.readbbl import read_bbl
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s %(name)-12s %(levelname)-5s] %(message)s')
 
-parser = argparse.ArgumentParser(description='Convert CCS book latex into HTML')
+parser = argparse.ArgumentParser(description='Convert CCS book latex into Quarto')
 parser.add_argument('chapters', type=int, nargs='*')
 args = parser.parse_args()
 
 bibliography = read_bbl("main.bbl")
 
 base = Path.cwd()
-out = base / "docs"
+out = base / "quarto"
 out.mkdir(exist_ok=True)
 toc = TOC(base)
-template = get_template('chapter.html')
+template = get_template('_quarto.yml')
+chapters = [x for x in toc.chapters if (not args.chapters) or (int(x.nr) in args.chapters)]
+(out / "_quarto.yml").open("w").write(template.render(**locals()))
 
-css_out = out / "ccsbook.css"
-if not css_out.exists():
-    css_in = Path(template.filename).parent / "ccsbook.css"
-    shutil.copy(css_in, css_out)
-
-#list(read_tex(base, "chapter08/test.tex"))
-#import sys; sys.exit()
 unknown = {}
 for chapnr, chapter in enumerate(toc.chapters, start=1):
     if args.chapters and chapnr not in args.chapters:
         continue
-    outf = out / chapter.fn
     prevchap = toc.chapters[chapnr-2] if chapnr > 1 else None
     nextchap = toc.chapters[chapnr] if chapnr < len(toc.chapters) else None
+    outf = out / chapter.fn
     print(f"{chapter.nr}: {chapter.texfile} -> {outf}")
     current_chapter = chapter.fn
     current_chapter_py = None if chapnr == 1 else f"chapter{chapnr:02}/chapter_{chapnr:02}_py.ipynb"
